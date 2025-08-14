6cf51e4b60cb5ac99af3fdce4d28fd941834abb2
shinwonkim <shinwonkkim@gmail.com>
2025-08-15 00:08:58 +0900

    feat: 리스트 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import TodoHeader from './Header/TodoHeader';
 import TodoInput from './Input/TodoInput';
+import TodoListTools from './Tools/TodoListTools';
 
 function App() {
   return (
     <main className="App">
       <TodoHeader></TodoHeader>
       <TodoInput></TodoInput>
+      <TodoListTools></TodoListTools>
     </main>
   );
 }
