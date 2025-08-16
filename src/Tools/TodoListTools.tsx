fdd6697a67838f21e9649556851a0632557bbbcb
shinwonkim <shinwonkkim@gmail.com>
2025-08-16 23:02:00 +0900

    fix: 스타일 클래스 중첩 문제 css modules로 해결

--- src/Tools/TodoListTools.tsx
+++ src/Tools/TodoListTools.tsx
@@ -1,8 +1,8 @@
-import "./TodolistTools.css";
+import styles from "./TodoListTools.module.css";
 
 const TodoListTools = () => {
     return (
-        <section className={"container"}>
+        <section className={styles.container}>
             <button>전체 완료</button>
             <button>전체 삭제</button>
         </section>
