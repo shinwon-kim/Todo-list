c5b59e595fedf663cbfb6c37796376e41633250a
shinwonkim <shinwonkkim@gmail.com>
2025-08-17 00:27:32 +0900

    feat: 리스트 레이아웃

--- src/Tools/TodoListTools.tsx
+++ src/Tools/TodoListTools.tsx
@@ -1,10 +1,15 @@
 import styles from "./TodoListTools.module.css";
+import { IoCheckmarkDoneCircleOutline } from "react-icons/io5";
+import { FaTrash } from "react-icons/fa6";
 
 const TodoListTools = () => {
     return (
         <section className={styles.container}>
-            <button>전체 완료</button>
-            <button>전체 삭제</button>
+            <button className={styles.button}>
+                {IoCheckmarkDoneCircleOutline({})}전체 완료</button>
+            <button className={styles.button}>
+                {FaTrash({}) }전체 삭제</button>
+                {/* <FaTrash/>전체 삭제</button> */}
         </section>
     )
 }
